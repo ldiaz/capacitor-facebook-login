405bcf3114bd93beb009c5782fb184a463cc1b1c
Leonardo Diaz <ingleonardo@gmail.com>
2019-07-16 09:24:24 -0500

    dist folder inclusion so when install as npm works as expected

---
+++ dist/esm/definitions.js
@@ -0,0 +1,18 @@
+export var FacebookEventName;
+(function (FacebookEventName) {
+    FacebookEventName["completedRegistration"] = "completedRegistration";
+    FacebookEventName["completedTutorial"] = "completedTutorial";
+    FacebookEventName["viewedContent"] = "viewedContent";
+    FacebookEventName["searched"] = "searched";
+    FacebookEventName["rated"] = "rated";
+    FacebookEventName["purchased"] = "purchased";
+    FacebookEventName["addedToCart"] = "addedToCart";
+    FacebookEventName["addedToWishlist"] = "addedToWishlist";
+    FacebookEventName["addedPaymentInfo"] = "addedPaymentInfo";
+    FacebookEventName["initiatedCheckout"] = "initiatedCheckout";
+    FacebookEventName["achievedLevel"] = "achievedLevel";
+    FacebookEventName["unlockedAchievement"] = "unlockedAchievement";
+    FacebookEventName["spentCredits"] = "spentCredits";
+})(FacebookEventName || (FacebookEventName = {}));
+//purchased: return "fb_mobile_purchase" // Hard-coded as a string, since it's internal API of FBSDKCoreKit.
+//# sourceMappingURL=definitions.js.map
